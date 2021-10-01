7055da0d23f46f43e48209a070681d39686e901c
David Trilla <david.trilla@ibm.com>
2021-10-01 06:17:26 -0400

    fixes to warnings in synthesis

--- src/nfu/src/nfu_config.sv
+++ src/nfu/src/nfu_config.sv
@@ -9,14 +9,11 @@
 module nfu_config #(
   parameter nfu_pkg::nfu_features_t         Features =  nfu_pkg::RV64NFU,
 
-  localparam int unsigned WIDTH              = Features.Width,
   localparam int unsigned ACCS               = Features.Accelerators,
   localparam int unsigned CONFIG_SELECTS     = Features.ConfigSelects,
   localparam int unsigned CONFIGS            = Features.Configs
 
 )( 
-  input logic                       clk_i,
-  input logic                       rst_ni,
   input logic [ACCS-1:0]            acc_i,
   input logic [CONFIGS-1:0]         config_i,
   // Input signals
@@ -26,10 +23,14 @@ module nfu_config #(
   // Output handshake
 );
 
-  reg [CONFIG_SELECTS-1:0] configs [ACCS-1:0][CONFIGS-1:0];
-  initial $readmemh("src/nfu/nfu.select.data", configs);
+  always_comb begin
+    case({acc_i,config_i}) 
+      8'b00000000 : select_o = 64'h8000000000000000;
+      8'b00000010 : select_o = 64'h4000000000000000;
+      default : select_o = 64'h0000000000000000;
+    endcase
+  end
 
-  assign select_o = configs[acc_i][config_i]; 
 
 
 endmodule
